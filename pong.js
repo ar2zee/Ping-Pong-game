96f678cde510e3866e04ee4a4aa8bfb53099256e
Artur Oganesyan <artur.og@yahoo.com>
2017-03-19 00:54:09 -0500

    Add files to Repository

---
+++ pong.js
@@ -0,0 +1,38 @@
+class Vec
+{
+	constructor(x = 0, y = 0)
+	{
+		this.x = x;
+		this.y = y;
+	}
+} 
+
+class Rect 
+{
+	constructor(w, h)
+	{
+		this.pos = new Vec;
+		this.size = new Vec(w, h);
+	}
+}
+
+class Ball extends Rect 
+{
+	constructor()
+	{
+		super(10, 10);
+		this.vel = new Vec;
+	}
+}
+
+ const canvas = document.getElementById('pong');
+ const context = canvas.getContext('2d');
+
+ const ball = new Ball;
+ console.log(ball);
+
+ context.fillStyle = '#000';
+ context.fillRect(0, 0, canvas.width, canvas.height)
+
+ context.fillStyle = '#fff';
+ context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
