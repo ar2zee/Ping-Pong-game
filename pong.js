920a11a3eeec1aef877b0c581152bdc0c8e4fcef
Artur Oganesyan <artur.og@yahoo.com>
2017-04-14 23:00:48 -0500

    Ball is moving now and bounce from edge

--- pong.js
+++ pong.js
@@ -30,9 +30,40 @@ class Ball extends Rect
 
  const ball = new Ball;
  console.log(ball);
+ ball.pos.x = 100;
+ ball.pos.y = 50;
 
- context.fillStyle = '#000';
- context.fillRect(0, 0, canvas.width, canvas.height)
+ ball.vel.x = 100;
+ ball.vel.y = 100;
 
- context.fillStyle = '#fff';
- context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
+ let lastTime;
+ function callback(millis){
+ 	if (lastTime) {
+ 		update((millis - lastTime) / 1000);
+ 	}
+ 	lastTime = millis;
+ 	requestAnimationFrame(callback);
+ }
+
+ function update(dt){
+ 	ball.pos.x += ball.vel.x * dt;
+ 	ball.pos.y += ball.vel.y * dt;
+
+ 	if (ball.pos.x < 0 || ball.pos.x > canvas.width){
+ 		ball.vel.x = -ball.vel.x;
+ 	}
+ 	if (ball.pos.y < 0 || ball.pos.y > canvas.height){
+ 		ball.vel.y = -ball.vel.y;
+ 	}
+
+ 	context.fillStyle = '#000';
+ 	context.fillRect(0, 0, canvas.width, canvas.height)
+	
+ 	context.fillStyle = '#fff';
+ 	context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
+
+ }
+
+ callback();
+
+ 
